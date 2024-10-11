628429f26eae4c4deffed96dda7848eac22b7b25
redkale <redkale@qq.com>
2024-10-11 12:27:23 +0800

    readField

--- src/main/java/org/redkale/demo/file/ImageRatio.java
+++ src/main/java/org/redkale/demo/file/ImageRatio.java
@@ -177,7 +177,7 @@ public final class ImageRatio {
                 int index = 0;
                 final Object[] params = new Object[deMembers.length];
                 while (in.hasNext()) {
-                    DeMember member = in.readFieldName(memberInfo); // 读取字段名
+                    DeMember member = in.readField(memberInfo); // 读取字段名
                     in.readBlank(); // 读取字段名与字段值之间的间隔符，JSON则是跳过冒号:
                     if (member == null) {
                         in.skipValue(); // 跳过不存在的字段的值, 一般不会发生
